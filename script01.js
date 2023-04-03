2b889c02b030937e4c9a363f515dd1d5904fac25
RG Mickael <rgmickael@gmail.com>
2023-04-03 18:54:00 +0300

    1st push, variables, operation and conditional logic

---
+++ script01.js
@@ -0,0 +1,16 @@
+// BMI = mass / height^2
+// mass in kg and height in meter
+
+var johnMass = 100.5;
+var johnHeight = 163.6;
+
+var markMass = 68;
+var markHeight = 172;
+
+var johnBMI = johnMass / (johnHeight**2);
+var markBMI = markMass / (markHeight**2);
+
+var isJohnBMIGreater = johnBMI > markBMI; 
+
+console.log("is John BMI greater than Mark? " + isJohnBMIGreater);
+
---
+++ script02.js
@@ -0,0 +1,47 @@
+var mattTeamScores = 2 + 0 + 0;
+var lukeTeamScores = 1 + 0 + 0;
+var maryTeamScores = 1 + 0 + 0;
+ 
+var averageScoreLuke = lukeTeamScores / 3;
+var averageScoreMatt = mattTeamScores / 3;
+var averageScoreMary = maryTeamScores / 3;
+
+//only Luke and Matt
+
+/*if ( averageScoreMatt > averageScoreLuke){
+	console.log("Matt's team win by "+ averageScoreMatt+" points");
+}else if (averageScoreMatt == averageScoreLuke){
+	console.log("There is a draw between Matt's team and Luke's by "+ averageScoreMatt+" points");
+}else{
+	console.log("Luke's team win by "+ averageScoreLuke+" points");
+}*/
+
+//all three
+
+if ((averageScoreMatt > averageScoreLuke) && (averageScoreMatt > averageScoreMary)) {
+	console.log("Matt's team win by "+ averageScoreMatt+" points");
+}else if (( averageScoreLuke > averageScoreMatt) && (averageScoreLuke > averageScoreMary)) {
+	console.log("Luke's team win by "+ averageScoreLuke+" points");
+}else if (( averageScoreMary > averageScoreMatt) && (averageScoreMary > averageScoreLuke)) {
+	console.log("Mary's team win by "+ averageScoreMary+" points");
+}else if (( averageScoreMatt == averageScoreLuke) && (averageScoreMatt == averageScoreMary)) {
+	console.log("There is a draw by "+ averageScoreMatt+" points");
+}else{
+	//look up for two draw
+	switch(true) {
+		case (averageScoreMatt == averageScoreLuke) && (averageScoreMatt > averageScoreMary):
+				console.log("There is a draw between Matt and Luke teams they win by "+averageScoreMatt+" points over Mary's");
+				break;
+		case (averageScoreMatt == averageScoreMary) && (averageScoreMatt > averageScoreLuke):
+				console.log("There is a draw between Matt and Mary teams they win by "+averageScoreMatt+" points over Luke's");
+				break;
+		case (averageScoreLuke == averageScoreMary) && (averageScoreLuke > averageScoreMatt):
+				console.log("There is a draw between Luke and Mary teams they win by "+averageScoreLuke+" points over Matt's");
+				break;
+		default:
+				console.log("Unable to process");
+				break;
+	}
+}
+
+
